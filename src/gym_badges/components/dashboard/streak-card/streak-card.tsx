ca6a9e973f11d8c7475d32c9e1503d9cc5180740
DiegoRevenga <diegorevengagonzalez@gmail.com>
2024-08-21 16:20:05 +0200

    Add Heroicons to the project.

--- src/gym_badges/components/dashboard/streak-card/streak-card.tsx
+++ src/gym_badges/components/dashboard/streak-card/streak-card.tsx
@@ -1,13 +1,17 @@
 import styles from "@/components/dashboard/streak-card/streak-card.module.scss"
+import { FireIcon } from "@heroicons/react/24/solid"
 
 export default function StreakCard() {
   return (
     <div className={styles.streak_card}>
       <h2>Your Streak</h2>
-      <p>33 weeks</p>
+      <div className={styles.streak_count}>
+        <FireIcon className={styles.icon}/>
+        <p>33 weeks</p>
+      </div>
       <p>This week</p>
       <p>[] [] [] [] [] [] []</p>
-      <p>Mon Tue Wed Thu</p>
+      <p>Mon Tue Wed Thu Fri Sat Sun</p>
     </div>
   );
 }
