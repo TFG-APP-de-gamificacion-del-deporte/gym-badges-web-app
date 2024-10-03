b2672a84cd79f9f87ca9d9597ff753b465d9f5e0
Diego Revenga Gonzalez <diegorevengagonzalez@gmail.com>
2024-10-03 18:41:15 +0200

    Add account popover

--- src/gym_badges/components/header/account-button/account-button.tsx
+++ src/gym_badges/components/header/account-button/account-button.tsx
@@ -1,14 +1,34 @@
 "use client";
 
+import Link from "next/link";
 import styles from "./account-button.module.scss"
-import { UserIcon } from "@heroicons/react/24/solid";
+import { ArrowLeftStartOnRectangleIcon, Cog6ToothIcon, UserIcon } from "@heroicons/react/24/solid";
+import clsx from "clsx";
 
 export default function AccountButton() {
   return (
   <>
-    <button className={styles.box}>
+  {/* @ts-ignore */}
+    <button className={styles.button} popovertarget="account_popover">
       <UserIcon className={styles.icon}/>
     </button>
+
+    <div id="account_popover" className={styles.popover} popover="auto">
+      <div>
+        <Link href='/account' className={styles.user}>
+          <UserIcon className={styles.icon}/>
+          Username
+        </Link>
+        <button className={styles.menu_button}>
+          <Cog6ToothIcon className={styles.icon_small} />
+          Settings
+        </button>
+        <button className={clsx(styles.menu_button, styles.logout)}>
+          <ArrowLeftStartOnRectangleIcon className={styles.icon_small} />
+          Log out
+        </button>
+      </div>
+    </div>
   </>
   )
 }
