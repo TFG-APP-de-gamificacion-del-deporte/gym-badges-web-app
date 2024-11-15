f0c33f52165773b3ab3e7a048e698291c3d3157a
DiegoRevenga <diegorevengagonzalez@gmail.com>
2024-11-15 18:49:19 +0100

    Change User icon in nav

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -4,8 +4,14 @@ import AccountButton from "./account-button/account-button";
 import styles from "./header.module.scss";
 import LevelBar from "./level-bar/level-bar";
 import Nav from "../nav/nav";
+import Link from "next/link";
+import { cookies } from "next/headers";
+import { AUTH_USER_ID_KEY } from "@/config/API";
+import DefaultProfilePicture from "../default-profile-picture/default-profile-picture";
 
 export default function Header() {
+  const user_id = cookies().get(AUTH_USER_ID_KEY);
+
   return (
     <>
       <div className={styles.header}>
@@ -15,7 +21,11 @@ export default function Header() {
         </div>
         <div className={styles.account_container}>
           <LevelBar/>
-          <AccountButton/>
+          <Link href={`/user/${user_id?.value}`}>
+            <div className={styles.image_container}>
+              <DefaultProfilePicture/>
+            </div>
+          </Link>
         </div>
       </div>
       <LineSeparator/>
