2456bbd57c80ac19765f642f7ed3e6a1577fdd34
Diego Revenga González <diegorevengagonzalez@gmail.com>
2024-12-12 16:01:36 +0100

    Color achieved badges

--- src/components/badge/badge.tsx
+++ src/components/badge/badge.tsx
@@ -2,6 +2,7 @@
 
 import { FaCheck, FaPlus, FaX, FaXmark } from "react-icons/fa6"
 import styles from "./badge.module.scss"
+import clsx from "clsx";
 
 export interface BadgeInfo {
   id: number,
@@ -24,7 +25,7 @@ export default function Badge({ badgeInfo, tooltip=true }: { badgeInfo: BadgeInf
           <FaXmark size="1.5rem"/>
         </button></header>
         {/* eslint-disable-next-line @next/next/no-img-element */}
-        <img src={`/badge-icons/${badgeInfo.image}`} alt={badgeInfo.id.toString()} draggable={false}/>
+        <img src={`/badge-icons/${badgeInfo.image}`} className={badgeInfo.achieved ? styles.achieved : ""} alt={badgeInfo.id.toString()} draggable={false}/>
         <h2>{badgeInfo.name}</h2>
         <div className={styles.buttons}>
           <button className={badgeInfo.achieved ? styles.button_unmark : ""}>
@@ -44,7 +45,7 @@ export default function Badge({ badgeInfo, tooltip=true }: { badgeInfo: BadgeInf
 
       {/* BADGE */}
       {/* @ts-ignore  */}
-      <button className={styles.badge} popovertarget={popoverId}>
+      <button className={clsx(styles.badge, {[styles.achieved]: badgeInfo.achieved})} popovertarget={popoverId}>
         {/* TOOLTIP */}
         {tooltip && <span className={styles.tooltip}>{badgeInfo.name}</span>}
         {/* ICON */}
