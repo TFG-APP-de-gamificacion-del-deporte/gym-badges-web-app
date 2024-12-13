65419ccdcc1c46adbee9e3d4f0c62b57207c92db
Diego Revenga González <diegorevengagonzalez@gmail.com>
2024-12-13 19:42:18 +0100

    Get stats widget info from the API

--- src/components/dashboard/stats-card/stats-card.tsx
+++ src/components/dashboard/stats-card/stats-card.tsx
@@ -1,8 +1,17 @@
+"use client"
+
 import clsx from "clsx"
 import styles from "./stats-card.module.scss"
+import useUser from "@/utils/useUser";
+import { redirect } from "next/navigation";
 
 
 export default function StatsCard() {
+  // Get user info
+  const { user, error, isLoading } = useUser();
+
+  if (error) redirect("/internal-error");
+
   return (
     <div className={styles.stats_card}>
       <h2>Stats</h2>
@@ -11,14 +20,14 @@ export default function StatsCard() {
         {/* WEIGHT BOX */}
         <div className={clsx(styles.box, styles.green_border)}>
           <div>
-            <h3>{88.5} kg</h3>
+            <h3>{user?.weight} kg</h3>
             <p>Weight</p>
           </div>
         </div>
         {/* FAT BOX */}
         <div className={clsx(styles.box, styles.yellow_border)}>
           <div>
-            <h3>{18}%</h3>
+            <h3>{user?.body_fat}%</h3>
             <p>Fat</p>
           </div>
         </div>
