c9e7f439030fa53f22147f586a632f4b339b692e
Diego Revenga González <diegorevengagonzalez@gmail.com>
2025-02-16 14:10:22 +0100

    Fix login msg and add friend button

--- actions/login.ts
+++ actions/login.ts
@@ -35,7 +35,7 @@ export default async function loginAction(prevState: any, formData: FormData): P
 
     // Invalid login
     if (!res.ok) {
-      return { message: "Invalid email or password." }
+      return { message: "Invalid username or password." }
     }
 
     console.debug("User loged in");
